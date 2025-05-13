0a02b7549c7502eca2811ebec32773e312bb464e
Joci0869 <gergo.papa00@gmail.com>
2025-05-13 20:23:31 +0200

    Added procedure to createBooking

---
+++ createBooking.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE PROCEDURE CreateBooking (
+    p_felhasznalo_id IN NUMBER,
+    p_jegy_id IN NUMBER,
+    p_datum IN DATE,
+    p_statusz IN VARCHAR2
+) AS
+    v_foglalva NUMBER;
+    v_next_id NUMBER;
+BEGIN
+    SELECT foglalva INTO v_foglalva FROM Jegy WHERE jegy_id = p_jegy_id;
+
+    IF v_foglalva = 1 THEN
+            RAISE_APPLICATION_ERROR(-20001, 'A jegy már foglalt.');
+    END IF;
+
+    SELECT NVL(MAX(foglalas_id), 0) + 1 INTO v_next_id FROM Foglalas;
+
+    INSERT INTO Foglalas (foglalas_id, felhasznalo_id, jegy_id, datum, statusz)
+    VALUES (v_next_id, p_felhasznalo_id, p_jegy_id, p_datum, p_statusz);
+END;
